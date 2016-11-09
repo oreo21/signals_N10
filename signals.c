42fccdf1b71abe0addc57c634ee3a20f64dbe225
oreo21 <reok99@gmail.com>
2016-11-09 15:33:26 -0500

    signals.c works now!

--- signals.c
+++ signals.c
@@ -8,14 +8,17 @@ void sighandler(int signum){
     printf("\nreceived signal %d, exiting...\n", signum);
     exit(0);
   }
+  
   if (signum == 10){
     printf("\nParent PID: %d\n", getppid());
     kill(getpid(), SIGCONT);
   }
+  
 }
 
 int main(){
   signal(SIGINT, sighandler);
+  signal(SIGUSR1, sighandler);
   while (1){
     printf("PID: %d\n", getpid());
     sleep(1);
