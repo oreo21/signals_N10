6226a587b60f6a25e45524b29ad7b515cb17d780
oreo21 <reok99@gmail.com>
2016-11-09 15:30:51 -0500

    initial commit:

---
+++ signals.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+void sighandler(int signum){
+  if (signum == 2){
+    printf("\nreceived signal %d, exiting...\n", signum);
+    exit(0);
+  }
+  if (signum == 10){
+    printf("\nParent PID: %d\n", getppid());
+    kill(getpid(), SIGCONT);
+  }
+}
+
+int main(){
+  signal(SIGINT, sighandler);
+  while (1){
+    printf("PID: %d\n", getpid());
+    sleep(1);
+  }
+  return 0;
+}
